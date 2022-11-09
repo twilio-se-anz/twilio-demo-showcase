256f5cb4ba780329d2f3c916d28218ec92149df0
Christopher Connolly <425070+chaosloth@users.noreply.github.com>
2022-11-09 15:49:58 +1100

    Add analytics and env example

--- common/types.ts
+++
@@ -1,9 +0,0 @@
-export type PluginListing = {
-  name: string;
-  category: string;
-  description: string;
-  image: string;
-  repo: string;
-  tags: string[];
-  owner: string;
-};
--- components/PluginListingCard/index.tsx
+++ components/PluginListingCard/index.tsx
@@ -1,17 +1,21 @@
 import {
+  Alert,
   Avatar,
   Badge,
   Box,
+  Text,
   Card,
   Heading,
   Paragraph,
   Stack,
+  Anchor,
 } from "@twilio-paste/core";
 
 import Link from "next/link";
 
 import { UserIcon } from "@twilio-paste/icons/cjs/UserIcon";
-import { PluginListing } from "../../common/types";
+import { PluginListing } from "../../serverless/functions/api/data";
+import { useAnalytics } from "../Analytics";
 
 export interface MediaCardProps {
   listing: PluginListing;
@@ -20,6 +24,8 @@ export interface MediaCardProps {
 export const PluginListingCard: React.FC<MediaCardProps> = (
   props: MediaCardProps
 ) => {
+  const analytics = useAnalytics();
+
   return (
     <Card padding="space50">
       <Stack orientation={"vertical"} spacing={"space50"}>
@@ -42,6 +48,24 @@ export const PluginListingCard: React.FC<MediaCardProps> = (
         <Heading as={"div"} variant={"heading30"}>
           {props.listing.name}
         </Heading>
+
+        {(props.listing.products || props.listing.flags) && (
+          <Box display="flex" columnGap="space80">
+            {props.listing.products &&
+              props.listing.products.map((product) => (
+                <Badge key={product} as="span" variant="new">
+                  {product}
+                </Badge>
+              ))}
+            {props.listing.flags &&
+              props.listing.flags.map((flag) => (
+                <Badge key={flag} as="span" variant="error">
+                  {flag}
+                </Badge>
+              ))}
+          </Box>
+        )}
+
         <Stack orientation={"horizontal"} spacing={"space30"}>
           <Avatar size="sizeIcon50" name="" icon={UserIcon} />
           <p>{props.listing.owner}</p>
@@ -53,7 +77,15 @@ export const PluginListingCard: React.FC<MediaCardProps> = (
         </Paragraph>
 
         {props.listing.repo && (
-          <Link href={props.listing.repo}>Visit Repository</Link>
+          <Anchor
+            target={"_blank"}
+            href={props.listing.repo}
+            onClick={() => {
+              analytics.track("View Listing", { ...props.listing });
+            }}
+          >
+            Find the code here
+          </Anchor>
         )}
 
         {props.listing.tags && (
